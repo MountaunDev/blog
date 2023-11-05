380faa8c155bea149d74875d5a206359669094bb
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2023-11-05 02:16:13 -0500

    Add the global css files into the root layout so they are working in all the pages without importing them again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import "@/app/app.scss";
-import Header from "../common/components/Header/Header";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Breadcrumb from "../common/components/Breadcumb/Breadcrumb";
-import BlogGridOne from "./blog/components/GridOne";
-
 export default function Home() {
   return (
    <h1>Main Page</h1>
