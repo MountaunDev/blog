a6ab21d7826969ae274ce6c4a29b8ba81d8e753e
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2023-11-19 11:26:54 -0500

    Make the posts page as a client component because we were having issues re-fetching the data using server components

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,24 +1,33 @@
+"use client";
 import Header from "@/common/components/Header/Header";
 import Breadcrumb from "@/common/components/Breadcumb/Breadcrumb";
 import PostList from "@/app/blog/components/PostList";
 import { fetchBlogPosts } from "../services/blogService";
+import { useEffect, useState } from "react";
+import { IModifiedBlogPostFields } from "@/types/blog";
 
-export default async function BlogHome() {
-  const data = await fetchBlogPosts();
+export default function BlogHome() {
+  const [data, setData] = useState<IModifiedBlogPostFields[]>();
+
+  useEffect(() => {
+    (async function () {
+      const data = await fetchBlogPosts();
+      setData(data);
+    })();
+  }),
+    [];
 
   return (
     <>
       <main className="main-wrapper">
         <Header />
-        <h3 className="title">
-              {JSON.stringify(data, null, 4)}
-          </h3>
+        <h3 className="title">{JSON.stringify(data, null, 4)}</h3>
         <Breadcrumb title="Blog" page="Blog" />
         <div className="section-padding-equal">
           <div className="container">
             <div className="row row-40">
               <div className="col-lg-8">
-                <PostList blogData={data} />
+                {data && <PostList blogData={data} />}
               </div>
               <div className="col-lg-4"></div>
             </div>
