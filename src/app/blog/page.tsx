23b332a02adc8dadca53d06506fa0bb59c0b5453
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2023-12-14 18:31:29 -0500

    Feat: Implement the new design in the PostList . - Created BlogSideBar component to render the right panel in the blogGrid page.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,23 +1,29 @@
 "use client";
+import React from "react";
+import { useEffect, useState } from "react";
+import BlogSidebar from "./components/BlogSideBar";
+import PostList from "@/app/blog/components/PostList";
 import Header from "@/common/components/Header/Header";
 import Breadcrumb from "@/common/components/Breadcumb/Breadcrumb";
-import PostList from "@/app/blog/components/PostList";
 import { fetchBlogPosts } from "../services/blogService";
-import { useEffect, useState } from "react";
+
+import ReactPaginate from "react-paginate";
 import { IFetchBlogPostsResponse } from "@/types/blog";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
-import ReactPaginate from "react-paginate";
-import PostListItemSkeleton from "./components/PostListItemSkeleton";
 
-const PAGE_SIZE = 2;
+const PAGE_SIZE = 4;
 
 export default function BlogHome() {
   const [data, setData] = useState<IFetchBlogPostsResponse>();
 
   const [pageNumber, setPageNumber] = useState(0);
+  const postsContainerRef = React.useRef<any>(null);
 
   const fetchMoreEntries = async ({ selected }: { selected: number }) => {
     setPageNumber(selected);
+    if (postsContainerRef.current) {
+      postsContainerRef.current.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   useEffect(() => {
@@ -36,30 +42,33 @@ export default function BlogHome() {
       <main className="main-wrapper">
         <Header />
         <Breadcrumb title="Blog" page="Blog" />
-        <div className="section-padding-equal">
-          <div className="container">
-            <div className="row row-40">
-              <div className="col-lg-8">
-                {!data ? (
-                  <PostListItemSkeleton />
-                ) : (
-                  <PostList blogData={data.items} />
-                )}
-                <ReactPaginate
-                  previousLabel={<FaArrowLeft />}
-                  nextLabel={<FaArrowRight />}
-                  pageCount={Math.ceil(data.total / PAGE_SIZE)}
-                  onPageChange={fetchMoreEntries}
-                  containerClassName={"pagination justify-content-start"}
-                  previousLinkClassName={"prev"}
-                  nextLinkClassName={"next"}
-                  disabledClassName={"disabled"}
-                  activeClassName={"current"}
-                />
+        <div className="section-padding-equal" ref={postsContainerRef}>
+          <section className="agency blog blog-sec blog-sidebar">
+            <div className="container">
+              <div className="row row-40">
+                <div className="col-lg-9">
+                  <div className="row">
+                    {/* TODO: Replace null for the skeletonLoader component */}
+                    {!data ? null : <PostList blogData={data.items} />}
+                  </div>
+                  <ReactPaginate
+                    previousLabel={<FaArrowLeft />}
+                    nextLabel={<FaArrowRight />}
+                    pageCount={Math.ceil(data.total / PAGE_SIZE)}
+                    onPageChange={fetchMoreEntries}
+                    containerClassName={"pagination justify-content-start"}
+                    previousLinkClassName={"prev"}
+                    nextLinkClassName={"next"}
+                    disabledClassName={"disabled"}
+                    activeClassName={"current"}
+                  />
+                </div>
+                <div className="col-lg-3">
+                  <BlogSidebar />
+                </div>
               </div>
-              <div className="col-lg-4"></div>
             </div>
-          </div>
+          </section>
         </div>
       </main>
     </>
