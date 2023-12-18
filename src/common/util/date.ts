351be1b9218271bd4f97038d662a6110129a17a3
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2023-12-17 23:04:00 -0500

    Fix error on build

--- src/common/util/date.ts
+++ src/common/util/date.ts
@@ -1,18 +1,14 @@
-export function extractDate_dd_mm_yyyy(dateTimeString) {
+export function extractDate_dd_mm_yyyy(dateTimeString: string): string | null {
   try {
     const dateObj = new Date(dateTimeString);
-
-    if (isNaN(dateObj)) {
-      throw new Error("Invalid date");
-    }
 
     const year = dateObj.getFullYear();
     const month = String(dateObj.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
     const day = String(dateObj.getDate()).padStart(2, "0");
 
     return `${day}-${month}-${year}`;
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error extracting date:", error.message);
-    return null; // Or handle the error in an appropriate manner for your application
+    return null;
   }
 }
